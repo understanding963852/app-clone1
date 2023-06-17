70cc3c3fba8dcc585011dc125b6f1f8830829db2
understanding963852 <60366769+understanding963852@users.noreply.github.com>
2023-06-17 17:15:19 +0900

    Add files via upload

---
+++ js1/main.js
@@ -0,0 +1,18 @@
+//css에서 모든 .sticky-elem을 fixed 처리했기때문에  
+//해당하는 영역에 진입하면 그 영역의 sticky-elem만 나오도록 한다
+//(function(){})()//즉시실행함수  --> 이렇게 사용하는 이유는 전역변수를 사용하는것을 피하기 위해서이다.
+
+(()=>{
+
+    
+  function setLayout() {
+    //각 영역의 높이값 세팅
+    for(let i=0; i<sceneInfo.length;i++){
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      sceneInfo[i].objs.container.style.height=`${sceneInfo[i].scrollHeight}px`
+    }
+  }
+  console.log(sceneInfo)
+    setLayout();
+
+})()
